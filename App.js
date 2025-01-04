c98a8f1a97514167e49a24782f843847c314aa77
Keerthivasan Mani <keerthivasan.mani1604@outlook.com>
2025-01-04 17:45:51 +0530

    Created a simple functional component and Updated the content of README file

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const h1 = (
-  <header className="header-section">
-    <h1 id="header">Hello React using JSX</h1>
-  </header>
-);
+const COURSEID = 174640;
+
+const Title = () => <h1>React JS Introdution</h1>;
+
+const Header = () => {
+  return (
+    <header className="header-section">
+      <Title />
+      <Title></Title>
+      {Title()}
+      <p>React JS is a open source library for building user interface</p>
+      <p>Course ID: {COURSEID}</p>
+    </header>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(h1);
+root.render(<Header />);
