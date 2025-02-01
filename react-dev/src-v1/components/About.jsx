000b4c5b5dfca82b13a34d832ffd23ed9f44d6ed
Keerthivasan Mani <keerthivasan.mani1604@outlook.com>
2025-02-01 11:10:30 +0530

    Updated react episode-2 study notes and renamed backup folder

---
+++ react-dev/src-v1/components/About.jsx
@@ -0,0 +1,11 @@
+import Example from "../classy/Example";
+
+const About = () => {
+  return (
+    <>
+      <Example name={"keerthivasan"} location={"Trichy"}/>
+    </>
+  );
+};
+
+export default About
---
+++ react-dev/timestamp.md
@@ -0,0 +1,6 @@
+
+## Episode 2
+- 00.00 Git Configuration
+- 15.35 Node Package Manager
+
+- 1.15.00 to continue...
